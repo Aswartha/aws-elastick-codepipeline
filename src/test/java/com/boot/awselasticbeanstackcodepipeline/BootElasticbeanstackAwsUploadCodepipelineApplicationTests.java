04e18af7980c04861e3db740826109ee6ab10f94
aswartha <palla.aswartha@gmail.com>
2021-02-23 14:41:29 +0530

    First commit

---
+++ src/test/java/com/boot/awselasticbeanstackcodepipeline/BootElasticbeanstackAwsUploadCodepipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.awselasticbeanstackcodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootElasticbeanstackAwsUploadCodepipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
